e13cc35ef89df6970ba0831d406367227bf84475
Alendia <alendia0807@gmail.com>
2021-06-18 21:25:42 +0800

    Finish

--- index.js
+++ index.js
@@ -2,63 +2,59 @@ const axios = require("axios").default;
 const HttpsProxyAgent = require("https-proxy-agent");
 const [targetLanguage, queryString] = process.argv.slice(2);
 
-let result = "";
+let result = [];
 
 function getUrl(tl, qry, tk) {
   const url = `https://translate.google.com/translate_a/single?client=webapp&sl=auto&tl=${tl}&hl=en&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=sos&dt=ss&dt=t&ssel=0&tsel=0&kc=1&tk=${tk}&q=${qry}`;
   return url;
 }
 
+function getResult(result, resp) {
+  result[0] = "";
+  for (const x of resp[0]) {
+    if (x[0]) {
+      result[0] += x[0];
+    }
+  }
+}
+
 function getSynonym(result, resp, queryString) {
+  result[1] = "";
   if (resp[1]) {
-    result = result.concat("\n=========\n", `0_0: Translations of ${queryString}\n`);
+    result[1] = result[1].concat("\n=========\n", `0_0: Translations of ${queryString}\n`);
     for (const x of resp[1]) {
-      result += `# ${x[0][0]}.\n`;
+      result[1] += `# ${x[0][0]}.\n`;
       for (const y of x[2]) {
-        console.log("getSynonym:", y[1]);
-        // result += `${y[0]}: ${", ".concat(y[1])}\n`;
+        result[1] += `${y[0]}: ${y[1].join(", ")}\n`;
       }
     }
   }
-  return result;
-}
-
-function getResult(result, resp) {
-  for (const x of resp[0]) {
-    if (x[0]) {
-      result += x[0];
-    }
-  }
-  return result + "\n";
-}
-
-function getDefinition(result, resp, queryString) {
-  result = result.concat("\n=========\n", `0_0: Definitions of ${queryString}\n`);
-  for (const x of resp[12]) {
-    result += `# ${x[0] || ""}.\n`;
-    for (const y of x[1]) {
-      result = result.concat(`  - ${y[0]}\n`, `${y.length >= 3 ? `    * ${y[2]}\n` : ""}`);
-    }
-  }
-  return result;
-}
-
-function getExamples(result, resp, queryString) {
-  result = result.concat("\n=========\n", `0_0: Examples of ${queryString}\n`);
-  for (const x of resp[13][0]) {
-    result += `  * ${x[0]}\n`;
-  }
-  return result;
 }
 
 function getSynonymEn(result, resp, queryString) {
-  result = result.concat("\n=========\n", `0_0: Synonyms of ${queryString}\n`);
+  result[2] = "".concat("\n=========\n", `0_0: Synonyms of ${queryString}\n`);
   for (const x of resp[11]) {
-    result += `# ${x[0]}.\n`;
+    result[2] += `# ${x[0]}.\n`;
     for (const y of x[1]) {
-      console.log("getSynonymEn:", y);
-      // result += `", ".concat`
+      result[2] += `${y[0].join(", ")}` + "\n";
     }
+  }
+}
+
+function getDefinition(result, resp, queryString) {
+  result[3] = "".concat("\n=========\n", `0_0: Definitions of ${queryString}\n`);
+  for (const x of resp[12]) {
+    result[3] += `# ${x[0] || ""}.\n`;
+    for (const y of x[1]) {
+      result[3] = result[3].concat(`  - ${y[0]}\n`, `${y.length >= 3 ? `    * ${y[2]}\n` : ""}`);
+    }
+  }
+}
+
+function getExamples(result, resp, queryString) {
+  result[4] = "".concat("\n=========\n", `0_0: Examples of ${queryString}\n`);
+  for (const x of resp[13][0]) {
+    result[4] += `  * ${x[0]}\n`;
   }
 }
 
@@ -69,22 +65,51 @@ function getResp(url) {
   });
 }
 
+function resultToHtml(result) {
+  const css = `<style type="text/css">
+  p {white-space: pre-wrap;}
+  pos {color: #0000FF;}
+  example {color: #008080;}
+  gray {color: #606060;}
+  </style>`;
+  result = result.join("");
+  // 词性
+  result = result.replace(/(#.*)/gi, "<pos><b>$1</b></pos>");
+  // 例子
+  result = result.replace(/([*].*)/gi, "<example>$1</example>");
+  result = result.replace(/(0_0:.*?of)(.*)/gi, "<gray>$1</gray>$2");
+  result = result.replace(/(^_^:\sTranslate)(.*)(To)(.*)/giu, "<gray>$1</gray>$2<gray>$3</gray>$4");
+  let html = `<html>\n<head>\n${css}\n</head>\n<body>\n<p>${result}</p></body>\n</html>`;
+  console.log(html);
+}
+
 async function getTranslation(targetLanguage, queryString) {
   const tk = calculateToken(queryString);
   const url = getUrl(targetLanguage, queryString, tk);
   try {
     const resp = await getResp(url);
-    console.log(resp.data);
     getResult(result, resp.data);
     getSynonym(result, resp.data, queryString);
 
+    if (resp.data[11]) {
+      getSynonymEn(result, resp.data, queryString);
+    }
+
+    if (resp.data[12]) {
+      getDefinition(result, resp.data, queryString);
+    }
+
+    if (resp.data[13]) {
+      getExamples(result, resp.data, queryString);
+    }
+    resultToHtml(result);
   } catch (error) {
     console.log(error);
   }
 }
 
 function Main() {
-  console.log(getTranslation(targetLanguage, queryString));
+  getTranslation(targetLanguage, queryString);
 }
 
 Main();
@@ -117,7 +142,7 @@ function workToken(firstSeed, seed) {
   for (let i = 0; i < seed.length - 2; i += 3) {
     let char = seed[i + 2];
     let d = parseInt(char, 16);
- 
+
     if (seed[i + 1] === "+") {
       d = firstSeed >>> d;
     } else {
